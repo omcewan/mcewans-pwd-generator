ec529a4161cf3212d7e53f67f6dc48f722d8f0d6
omcewan <orlandomcewan231@gmail.com>
2021-12-18 17:20:50 -0500

    add prompts to ask for type of characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,14 @@ var passwordLength = 8;
 // function for choosing the length of the password
 var critieriaForLenght = function(){
   // ask the user to choose a length for their password
-  var userChosenLength = window.prompt('Choose a length for your password', 8);
+  var userChosenLength = window.prompt('Choose a length for your password. Must be at least 8 characters, no longer than 128.', 8);
   // check to see if the choosen number is within the parameter range
   if (userChosenLength >= 8 && userChosenLength <= 128){
     // ask the user to confirm the length chosen.
     var userConfirmedLength = window.confirm('Are you sure this is the lenght you want for your password?');
   }
   else {
-    var userChosenLength = window.prompt('Choose a length for your password', 8);
+    var userChosenLength = window.prompt('Choose a length for your password. Must be at least 8 characters, no longer than 128.', 8);
   }
   // if length chosen is confirmed assign to passwordLength else run function over.
   if (userConfirmedLength){
@@ -28,27 +28,43 @@ var critieriaForLenght = function(){
   }
 }
 
-// Choose characters to include so will need a prompt for numbers, letters, special char, uppercase and/or lowercase.
-var typeOfCharacters = function(){
-  // prompt to ask if to include numbers
-  window.prompt('Would you like to include numbers?');
+// Choose characters to include so will need a prompt for numbers.
+var choiceOfNumbers = window.confirm('Would you like to include numbers?');
+if (choiceOfNumbers){
+  choiceOfNumbers = true;
+}
+else{
+  choiceOfNumbers = false;
+}
+console.log(choiceOfNumbers)
 
-  //  prompt to ask if to include lowercase letters
-  window.prompt('Would you like to include lowercase letter?');
-  
-  // prompt to ask if to inculde upper case letters
-  window.prompt('Would you like to include uppercase letters');
-
-  // prompt to ask if to include special characters
-  window.prompt('Would you like to include special characters');
-
+// //  prompt to ask if to include lowercase letters
+var choiceOfLowercase = window.confirm('Would you like to include lowercase letters?');
+if (choiceOfLowercase){
+  choiceOfNumbers = true;
+}
+else{
+  choiceOfLowercase = false;
 }
 
-// choices need to be validated for each prompt
+// // prompt to ask if to inculde upper case letters
+var choiceOfUppercase = window.confirm('Would you like to include Uppercase letters?');
+if (choiceOfUppercase){
+  choiceOfNumbers = true;
+}
+else{
+  choiceOfUppercase = false;
+}
 
-// password is then generated after prompts are confirmed.
+// // prompt to ask if to include special characters
+var choiceOfSpecial = window.confirm('Would you like to include Special letters?');
+if (choiceOfSpecial){
+  choiceOfNumbers = true;
+}
+else{
+  choiceOfSpecial = false;
+}
 
-// password is then displayed in an alert or in the text box.
 
 
 
@@ -57,12 +73,19 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 // function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+  //   var password = generatePassword();
+  //   var passwordText = document.querySelector("#password");
+  
+  //   passwordText.value = password;
+  
+  // }
+  
+  // Add event listener to generate button
+  // generateBtn.addEventListener("click", writePassword);
+  
+  // My Pseudo Code
+  // choices need to be validated for each prompt
+  
+  // password is then generated after prompts are confirmed.
+  
+  // password is then displayed in an alert or in the text box.
